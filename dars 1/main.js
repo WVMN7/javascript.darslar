79d6e961c0b37cd6958df1636061f19c868941de
WVMN <abbossabiryozov6@gmail.com>
2025-12-10 20:26:34 +0500

    Add files via upload

---
+++ dars 1/main.js
@@ -0,0 +1,57 @@
+// alert('Hello ')
+
+// O'zgaruvchilar
+
+// 2 xil o'zgaruvchilar
+
+// let age = 22
+// console.log(age);
+
+// age = 23
+// console.log(age);
+
+// const name = 'Gofur'
+// console.log(name);
+
+// name = 'Asad'
+// console.log(name);
+
+// const person = {
+//     lastName: 'Kamol'
+// }
+// console.log(person.lastName);
+
+// lastName = 'Asad'
+
+// console.log(lastName);
+
+// ESKI Usul
+
+// console.log(age);
+// var age = 22
+
+
+let age1 = 23
+console.log(age1);
+
+// age1 != Age1
+
+let Age1 = 32
+console.log(Age1);
+
+
+// uyga vazifa 
+// 01
+
+// let z = 1
+// x = z ** 3
+// console.log(x)
+
+// let y = 3
+// x = y ** 3
+// console.log(x)
+
+// 02
+// importScripts
+// let doira = 3
+// math (pi)
