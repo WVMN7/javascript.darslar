4aa7292b96b29078af635305c04da52aafc0d8d9
WVMN <abbossabiryozov6@gmail.com>
2025-12-10 20:27:17 +0500

    Add files via upload

---
+++ dars 3/mains.js
@@ -0,0 +1,52 @@
+// object - komplex malumit turlari
+// malumot turlari
+// number = 3
+// string  'dwdwwddwdw'
+// booling   true / false
+
+// const person = {
+//     name: Asad ,
+//     age: 23 ,
+//     job: Devoliper ,
+//     hobbies : {
+//         sport: football , 
+//         music: rep   
+//     } 
+// }
+// console.log(person);
+
+// const color = ['red', 'greene' , 'yellow']
+// console.log(color);
+
+// color.push('white')  // oxiriga qoshadi
+// console.log(color)
+
+// color.unshift('pink') // oldiga qoshadi
+// console.log(color)
+
+// color.pop() // oxiridan malumot o'chiradi
+// console.log(color);
+
+// color.shift()
+// console.log(color);
+
+const fruit = ['olam' , 'uzum', 'nok', 'olcha']
+console.log(fruit);
+
+fruit.push('Banan')
+console.log(fruit);
+
+fruit.unshift('anor')
+console.log(fruit);
+
+fruit.pop()
+console.log(fruit);
+
+fruit.shift()
+console.log(fruit);
+
+
+
+
+
+
