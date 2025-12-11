43e9392ad7a6cb10225b995e8960774017dbed19
WVMN <abbossabiryozov6@gmail.com>
2025-12-11 18:58:53 +0500

    Add files via upload

---
+++ dars 4/main.js
@@ -0,0 +1,131 @@
+//  windov Interpolatsiya va Operatorlar
+
+// alert('hello world')   //malumot berish oynasi
+
+// const age = confirm('18 yoshdan kattamisiz? ')
+// console.log(age);
+
+
+// const r =  prompt('Ismingiz kim?')  // Foydalanuvchidan malumot olish
+// console.log(r);
+
+// const age = +prompt('Yoshingiz nechada ?')  //+ str ni number qiladi.
+// console.log(age);
+
+// const firstName = 'Abbosbek'
+// const lastName = 'Sobiryozov'
+// const age = 15
+// console.log('My first name is ' + firstName + ', my last name is ' + lastName + ' ' + age);
+
+
+// console.log(`My name is ${firstName} , my last name is ${lastName}, i'm ${age} yers old`);
+
+
+// t
+// const i = prompt('Ismingizni kiriting ?')
+// const f = prompt('Familiyangizni kiriting ?')
+// const y = +prompt('Yoshingizni kiriting')
+
+// console.log(`Sizning ismingiz ${i} , familyangiz ${f}, yoshingiz ${y} `);
+
+// const w = prompt('nima olmoqchi siz ?')
+// const cola = 8000
+// const olma = 10000
+// const banan = 7000
+// const nok = 11000
+
+// console.log(`${cola + olma +banan +nok
+// }`);
+
+
+
+
+// Operatorlar
+
+// inferement
+// let x = 10
+// let y = 2 
+
+// ++x
+// --y
+// console.log(x);
+// console.log(y);
+
+// && va operotorlar *
+// || yoki operotorlar *
+
+//  Window Interpolatsiya va Operatorlar
+
+// alert('Hello world')    // malumot berish oynasi
+
+// const age = confirm('18 yoshan kattamisiz?')    // xa yoki yo'q / true and false
+// console.log(age);
+
+// const name = prompt('Ismingiz nima?')  // foydalanuvchidan malumot olish
+// console.log(name);
+
+// const age = +prompt('Yoshingiz nechada?')
+// console.log(age);
+// console.log(typeof age);
+
+// Interpalatsiya
+// const firstName = 'Gofur'
+// const lastName = 'Ruziev'
+// const age = 32
+
+// console.log('My name is ' + firstName + ', my lastname is ' + lastName +' '+ age);
+
+// console.log(`My name is ${firstName}, my last name is ${lastName} i'm ${age} yars old`);
+
+// console.log(`https://youtube/${}`);
+
+// const firstName = prompt('Ismingni yoz')
+// const lastName = prompt('Familyangni yoz')
+// const age = +prompt('Yoshingni kirit')
+
+// console.log(`Maning ismim ${firstName}, familyam ${lastName} va men ${age} yoshdaman`);
+
+// Operatorlar
+let y = 10
+let x = '10'
+
+// console.log(x + y); // qo'shish
+// console.log(x - y); // ayirish
+// console.log(x * y); // ko'paytirish
+// console.log(x / y); // bo'lish
+// console.log(x % y); // foiz
+// console.log(x ** y); // daraja
+// console.log(x < y);
+
+// console.log(x += y); // x = x + y
+// console.log(x -= y); // x = x - y
+// console.log(x *= y); // x = x * y
+// console.log(x /= y);  // x = x / y
+
+console.log(x == y); // tenglik
+console.log(x === y); // qattiy tenglik
+
+
+// Increment va Decrement
+
+// Posfix
+// x++ // 1 ga oshiradi
+// y-- // 1 ga kamaytiradi
+// console.log(x);
+// console.log(y);
+
+// Prefix
+// ++x
+// --y
+// console.log(x);
+// console.log(y);
+
+// && va operatorlar  *
+// || yoki operatorlar   +
+
+// const isMerrid = true
+// const hasChildren = false
+
+// console.log(isMerrid || hasChildren);
+
+
