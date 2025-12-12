002f7dafe2473c7da32595f1ee2127fa346c67bb
WVMN <abbossabiryozov6@gmail.com>
2025-12-12 20:21:05 +0500

    Add files via upload

---
+++ dars 7/main.js
@@ -0,0 +1,96 @@
+
+// while
+// let number = 1
+// while(number <= 10) {
+//     console.log(number);
+//     number++   
+// }
+
+// do while
+
+// let number = 1
+// do{
+//     console.log(number);
+//     number++
+// }
+// while (number <=10 )
+
+// for while
+
+// let number = 1
+// for (number; number <=10; number++){
+//     console.log(number);
+// }
+
+// let i = 1
+// for(i ; i <=10; i++){
+//     console.log(i);
+// }
+
+
+// let juft = 1
+// while(juft <=10){
+//     console.log(juft);
+//     juft++
+// }
+
+
+// for (let i = 2;i<=10; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+// let fruit = ['bnana', 'apple', 'orange']
+// for(frut of fruit){
+//     console.log(frut);
+    
+// }
+
+// let i = 10
+// for(i ; i > 0; i--){
+//     console.log(i);
+// }
+
+
+// let h = 0
+// for(let i = 1 ; i <=10; i++){
+//     h += i
+// }
+// console.log( h);
+
+// let n = 0
+// for (let i = 2;i<=10; i++){
+//     if(i % 2 === 0){
+//         n += i
+
+//     }
+// }
+// console.log(n);
+
+
+
+// for (let i = 1;i<=10; i++){
+//     if(i % 2 === 1 ){
+//         console.log(i);
+//     }
+// }
+
+// let = 0
+// for (let i = 1;i<=10; i++){
+//     if(i % 2 === 1 ){
+//         n += i
+//     }
+// }
+// console.log(n);
+
+
+// let num =[4,8,15,16,23,42]
+
+// let sum = 0
+
+// for(let n of num){
+//     sum +=n
+// }
+// console.log(sum/ num.length);
