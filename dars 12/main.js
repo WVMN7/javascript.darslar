fca93aa7bc28fb9a3243dfb97287320ac6410830
WVMN <abbossabiryozov6@gmail.com>
2025-12-15 20:42:01 +0500

    Add files via upload

---
+++ dars 12/main.js
@@ -0,0 +1,43 @@
+// massivlar
+
+// const color = ['red' , 'green', 'blue', 'yellow', 'pink']
+
+// console.log(color.length);
+// console.log(color[1]);
+
+// pop === masiv oxirgi elementini ochiradi
+// push === masiv oxiriga element qoshadi
+// shift === massiv boshidan element ochiradi
+// unshift === massiv boshiga element qoshadi
+
+// console.log(color.pop());
+// console.log(color);
+
+// console.log(color.push('orange'));
+
+
+// const color = ['red' , 'green', 'blue', 'yellow', 'pink']
+
+// interatsiya
+
+// 1 usul
+// for(let i = 0; i<color.length; i++){
+//     console.log(color[i]);
+// }
+
+// 2 usul
+// for (let rang of color){
+
+// }
+
+
+// split
+// const text = 'assalomu alaykum tekintamoq oquvchilar'
+// arr
+
+const color = ['red' , 'green', 'blue', 'yellow', 'pink']
+
+for(let i = 0; i<color.length; i++){
+    console.log(color[i]);
+    
+}
