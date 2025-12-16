7caf1b85a10eebb731472df103e9168730b312f9
WVMN <abbossabiryozov6@gmail.com>
2025-12-16 20:33:35 +0500

    Add files via upload

---
+++ dars 13/main.js
@@ -0,0 +1,35 @@
+
+// const color = ['red' , 'green', 'blue', 'yellow', 'pink']
+
+// for(let i = 0; i<color.length; i++){
+//     console.log(color[i]);
+    
+// }
+
+
+// const calc  = (a,b) => {
+//     switch (true) {
+//         case `+`: return a + b 
+//         case `-`: return a - b
+//         case `*`: return a * b
+//         case `/`: return a / b
+//         default : return 'notog;ri porometr'
+//     }
+    
+// }
+// console.log(calc(12, 5, '+'));
+// console.log(calc(22, 4, '-'));
+// console.log(calc(14, 7, '*'));
+// console.log(calc(16, 5, '/'));
+
+
+// const ism = prompt("Ismingizni kiriting:"),
+//  familya = prompt("Familyangizni kiriting:"),
+//  yil = +prompt("Nechanchi yilda tog'ilgansiz :")
+
+// console.log(`Siz ${ism} ${familya} va siz ${yil} - yilsiz va ${2025 - yil} yoshdasiz`);
+
+function dokon() {
+    const cola =  prompt("Cola olasizmi")
+
+}
