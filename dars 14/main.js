766b325e71c03e7421828ff3b25fe00443099714
WVMN <abbossabiryozov6@gmail.com>
2025-12-16 20:33:47 +0500

    Add files via upload

---
+++ dars 14/main.js
@@ -0,0 +1,24 @@
+// Array clonlash
+// const cars = ['BMW','Kia', 'GM', 'Chevrolet' ]
+
+// const clonArray = cars.slice()
+// clonArray.push('Suzuki')
+// clonArray[0] = 'Audi'
+// console.log(cars);
+// console.log(clonArray);
+
+// const son = [1,2,3,4]
+// const clonArray = son.slice()
+// clonArray.push(5)
+// console.log(son);
+// console.log(clonArray);
+
+
+
+const product = {
+    name: "Acer nitro",
+    sum: 500,
+    color: 'black'
+}
+
+const upperproduct = {...product}
