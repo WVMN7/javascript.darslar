9c0ca6492e1d4775fc18f4fb5053b86ea13fae69
WVMN <abbossabiryozov6@gmail.com>
2025-12-19 20:26:23 +0500

    Add files via upload

---
+++ dars 18/main.js
@@ -0,0 +1,41 @@
+// hodisalar 
+const btn = document.querySelector('button'),
+      btna = document.querySelector('.btn-a'),
+      btnb = document.querySelector('.btn-b')
+
+
+// console.log(btn);
+
+// btn.onclick = function(){
+//     console.log("siz birni tanladingiz ");
+// }
+
+// btn.onclick = function(){
+//     console.log("siz ikkinchini tanladingiz ");
+// }
+
+
+// btn.addEventListener('mouseenter', () =>{
+//     console.log('Siz birni tanladingiz');
+// })
+
+// btn.addEventListener('mouseleave', () =>{
+//     console.log('Siz ikkini tanladingiz');
+// })
+
+// btn.addEventListener('click', (e) =>{
+//     console.log(e.target);
+//     e.target.textContent = 'Button' 
+//     e.target.style.backgroundColor = 'blue'
+//     e.target.style.borderRadius = '15px'
+//     // e.target.remove()
+// })
+
+// btna.addEventListener('click', (e) => {
+//     e.target.remove()
+// })
+// btnb.addEventListener('click', (e) => {
+//     e.target.textContent = 'Button' 
+//     e.target.style.backgroundColor = 'red'
+//     e.target.style.borderRadius = '10px'
+// })
